8ad329fac63feb8d01a9b9b66f7c4eb264c543ba
AnaCarolinaCCH123 <ac521814@gmail.com>
2024-11-28 20:45:14 -0600

    web

--- src/schemas/ph.ts
+++ src/schemas/ph.ts
@@ -6,5 +6,5 @@ export const phSchema = z.object({
   valor_ph: z
     .number()
     .min(0, { message: "El valor de pH debe ser mayor o igual a 0" })
-    .max(14, { message: "El valor de pH no puede ser mayor a 14" }),
+    .max(100, { message: "El valor de pH no puede ser mayor a 100" }),
 });
